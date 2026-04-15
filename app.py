c5495fd2249c42adbece7d3ba46c0d3c3cd32de8
Praveen Ravi <146102182+ChampPraveen@users.noreply.github.com>
2026-04-15 11:04:43 +0530

    Update Hugging Face login token in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 app = FastAPI()
 
 from huggingface_hub import login
-login("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")
+login("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")
 # Load model
 model_path = "Praveen02072004/Alpaca-gpt2"
 
